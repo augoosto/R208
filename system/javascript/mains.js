34be6590e786ce539d8dcfd41bdbec930d4bac25
Augosto <agostc@proton.me>
2024-04-27 23:02:11 -0300

    R208 Diário

--- system/javascript/mains.js
+++ system/javascript/mains.js
@@ -1,5 +1,6 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 var currentdate = new Date(); 
 var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + " - " + currentdate.getDate() + "/" + (currentdate.getMonth()+1) + "," + currentdate.getFullYear() ;
 window.onload = function() { 
  document.getElementById("datetime").innerHTML = datetime;
-} 
+}
