5e64746767e86a67f89c6bdd057871a9e3629b4b
Augosto <agostc@proton.me>
2024-04-27 21:44:16 -0300

    Relatório Diário

---
+++ system/javascript/mains.js
@@ -0,0 +1,5 @@
+var currentdate = new Date(); 
+var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + " - " + currentdate.getDate() + "/" + (currentdate.getMonth()+1) + "," + currentdate.getFullYear() ;
+window.onload = function() { 
+ document.getElementById("datetime").innerHTML = datetime;
+} 
